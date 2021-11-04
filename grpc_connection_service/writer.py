d12af133267363ec98622bb5f7919c994a8b5c3c
Abbie Weisenbloom <Froggie@macbook-pro-3.lan>
2021-11-04 13:12:01 -0700

    Added grcp writer

--- grpc_connection_service/writer.py
+++ grpc_connection_service/writer.py
@@ -2,13 +2,10 @@ import grpc
 import connection_pb2 #reuse these files for client and server
 import connection_pb2_grpc
 
-"""
-Sample implementation of a writer that can be used to write messages to gRPC.
-"""
 print("Sending sample payload...")
 
 channel = grpc.insecure_channel("localhost:5005", options=(('grpc.enable_http_proxy', 0),))
-stub = connection_pb2_grpc.ConnectionServiceStub(channel)
+connections_client = connection_pb2_grpc.ConnectionServiceStub(channel)
 
 # Update this with desired payload. Create a message and set values ...
 item = connection_pb2.ConnectionMessage(
@@ -17,8 +14,6 @@ item = connection_pb2.ConnectionMessage(
     end_date="2020-12-30",
     meters=5,
 )
-response = stub.GetConnection(item)
-print("Greeter client received: " + response.message)
-# response = stub.Create(connection)  #pass connection to Create method
-# feature = stub.GetFeature(item)
-# print(item)
+connections_response = connections_client.GetConnection(item)
+print("Greeter client received: ... ")
+print(connections_response.connections)
